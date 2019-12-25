8dfddebbbfe67425976f662fa5fc1baefaa4fdc6
sonali mahavar <53106053+sonali-mahavar@users.noreply.github.com>
2019-12-25 11:12:08 +0530

    Add files via upload

---
+++ STRINGTOCHAR.java
@@ -0,0 +1,18 @@
+package stringtochar;
+
+/**
+ *
+ * @author sonali mahavar
+ */
+public class STRINGTOCHAR {
+impot java.util.Scanner;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s="HELLO";
+        char c=s.charAt(0);
+        System.out.println("1st character of the string is:"+c)
+    }
+    
+}
